ddcce408e4d86d1eef92853e47a34d06666382b5
Vunb <nhubaovu@gmail.com>
2019-09-23 19:25:34 +0700

    refactor webpack config to typescript

--- extensions/templates/html-conference.ts
+++ extensions/templates/html-conference.ts
@@ -2,6 +2,8 @@ const html = ({ body }: { body: string }) => `
   <!DOCTYPE html>
   <html>
     <head>
+      <title>RoomRTC Community Edition</title>
+      <link rel="icon" href="data:,">
     </head>
     <body style="margin:0">
       <div id="app">${body}</div>
---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import webpack from 'webpack';
+import path from 'path';
+import nodeExternals from 'webpack-node-externals';
+import WebpackShellPlugin from 'webpack-shell-plugin';
+
+const config = {
+  mode: 'development',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new WebpackShellPlugin({
+      onBuildEnd: ['npm run start:server'],
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    }],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: ['src', 'node_modules'],
+  },
+};
+
+const client = Object.assign({}, config, {
+  name: 'client',
+  target: 'web',
+  entry: path.resolve(__dirname, 'client/src/client-entry.tsx'),
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'build/assets'),
+  },
+});
+
+const server = Object.assign({}, config, {
+  name: 'server',
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: path.resolve(__dirname, 'app.ts'),
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+});
+
+module.exports = [client, server];
