84d5f5582452a5fc1b8ee1768a51fcefa53f7e16
Vunb <nhubaovu@gmail.com>
2019-09-30 22:32:44 +0700

    bundle css using gulp

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,31 +2,13 @@ import webpack from 'webpack';
 import path from 'path';
 import nodeExternals from 'webpack-node-externals';
 import WebpackShellPlugin from 'webpack-shell-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const config = {
   mode: 'development',
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.styl$/,
-          chunks: 'all',
-          enforce: true,
-        },
-      },
-    },
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new WebpackShellPlugin({
       onBuildEnd: ['npm run start:server'],
-    }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[app].css',
     }),
   ],
   module: {
@@ -34,25 +16,10 @@ const config = {
       test: /\.tsx?$/,
       use: 'ts-loader',
       exclude: /node_modules/,
-    }, {
-      test: /\.styl$/,
-      use: [MiniCssExtractPlugin.loader, 'stylus-loader'],
-      // use: [
-      //   {
-      //     loader: 'stylus-loader', // compiles Stylus to CSS
-      //     options: {
-      //       sourceMap: true,
-      //       import: [process.cwd(), path.resolve('client/src/stylus/index.styl')], // <-- add this
-      //       use: [
-      //         require('nib')(),
-      //       ],
-      //     },
-      //   },
-      // ],
     }],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.styl'],
+    extensions: ['.tsx', '.ts', '.js'],
     modules: ['src', 'node_modules'],
   },
 };
